7f0c796d8687b25ef1c24375fd06cb9f2c0c1d97
Anh Dung Bui <dungba.sg@gmail.com>
2019-04-11 16:59:28 +0700

    support custom constructor

--- boot/src/main/java/io/gridgo/boot/support/scanners/impl/ComponentScanner.java
+++ boot/src/main/java/io/gridgo/boot/support/scanners/impl/ComponentScanner.java
@@ -1,6 +1,7 @@
 package io.gridgo.boot.support.scanners.impl;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.reflections.Reflections;
@@ -10,6 +11,9 @@ import io.gridgo.boot.support.LazyInitializer;
 import io.gridgo.boot.support.annotations.Component;
 import io.gridgo.boot.support.exceptions.InitializationException;
 import io.gridgo.core.GridgoContext;
+import io.gridgo.core.support.exceptions.AmbiguousException;
+import io.gridgo.framework.support.Registry;
+import io.gridgo.framework.support.exceptions.BeanNotFoundException;
 
 public class ComponentScanner implements AnnotationScanner {
 
@@ -25,16 +29,41 @@ public class ComponentScanner implements AnnotationScanner {
         var annotation = comp.getAnnotation(Component.class);
         var name = annotation.value();
         try {
-            var instance = comp.getConstructor().newInstance();
+            var instance = resolveClass(comp, context);
             if (!name.isEmpty()) {
                 context.getRegistry().register(name, instance);
             } else {
                 context.getRegistry().register(comp.getName(), instance);
             }
             lazyInitializers.add(new LazyInitializer(comp, instance));
-        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-                | NoSuchMethodException | SecurityException e) {
+        } catch (IllegalArgumentException | SecurityException e) {
             throw new InitializationException("Cannot register processor", e);
         }
+    }
+
+    public Object resolveClass(Class<?> clazz, GridgoContext context) {
+        try {
+            var constructors = clazz.getConstructors();
+            if (constructors.length > 1)
+                throw new AmbiguousException("Only one constructor is allowed");
+            var constructor = constructors[0];
+            var params = Arrays.stream(constructor.getParameterTypes()) //
+                               .map(type -> lookupForType(context, type)) //
+                               .toArray(size -> new Object[size]);
+            return constructor.newInstance(params);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object lookupForType(GridgoContext context, Class<?> type) {
+        if (type == GridgoContext.class)
+            return context;
+        if (type == Registry.class)
+            return context.getRegistry();
+        var answer = context.getRegistry().lookupByType(type);
+        if (answer == null)
+            throw new BeanNotFoundException("Cannot find any bean with the required type " + type.getName());
+        return answer;
     }
 }

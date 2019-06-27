18d5e63c5f37aa78dcbac593c4690e068c7a9328
Phu Nguyen <hongphu8790@gmail.com>
2019-06-27 16:16:00 +0700

    @PostConstruct supported

--- boot/src/main/java/io/gridgo/boot/support/FieldInjector.java
+++ boot/src/main/java/io/gridgo/boot/support/FieldInjector.java
@@ -1,13 +1,18 @@
 package io.gridgo.boot.support;
 
-import java.util.Arrays;
-import java.util.List;
-
 import io.gridgo.boot.data.DataAccessInjector;
+import io.gridgo.boot.support.annotations.AnnotationUtils;
+import io.gridgo.boot.support.annotations.PostConstruct;
+import io.gridgo.boot.support.exceptions.InitializationException;
 import io.gridgo.boot.support.injectors.impl.ComponentInjector;
 import io.gridgo.boot.support.injectors.impl.GatewayInjector;
 import io.gridgo.boot.support.injectors.impl.RegistryInjector;
 import io.gridgo.core.GridgoContext;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
 
 public class FieldInjector {
 
@@ -24,6 +29,30 @@ public class FieldInjector {
     public void injectFields(Class<?> gatewayClass, Object instance) {
         for (var injector : injectors) {
             injector.inject(gatewayClass, instance);
+        }
+        postConstructInitialize(gatewayClass, instance);
+    }
+
+    /**
+     * Post constructor initialize
+     * @param gatewayClass The component class
+     * @param instance The instance of gatewayClass
+     */
+    private void postConstructInitialize(Class<?> gatewayClass, Object instance) {
+        var postConstructs = AnnotationUtils.findAllMethodsWithAnnotation(gatewayClass, PostConstruct.class);
+        if (postConstructs.size() >= 2) {
+            throw new InitializationException("Only one PostConstruct method in a component be used");
+        } else if (postConstructs.size() == 1) {
+            var postConstruct = postConstructs.get(0);
+            try {
+                if (Modifier.isStatic(postConstruct.getModifiers())) {
+                    postConstruct.invoke(null);
+                } else {
+                    postConstruct.invoke(instance);
+                }
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                throw new InitializationException("Cannot initialize the PostConstruct", e);
+            }
         }
     }
 }
---
+++ boot/src/main/java/io/gridgo/boot/support/annotations/PostConstruct.java
@@ -0,0 +1,12 @@
+package io.gridgo.boot.support.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(value = METHOD)
+@Retention(value = RUNTIME)
+public @interface PostConstruct {
+}
